df12312a9c603f10854ba482abf46b60fce13273
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-07-30 08:09:02 -0700

    also added messages for correct, incorrect guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 //Word bank that function pulls from 
 var wordBank = ["pig", "cow", "chicken", "sheep", 'horse', "monkey", "bird", "hawk", "snake", "fish", "spider", "cat", "human", "penguin"];
 //Current word that user must guess (random word from wordBank array)
-var currentWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 //Current word divided into letters
 var currentLetters = currentWord.split("");
 //Number of current letters (to be guessed)
@@ -12,7 +12,7 @@ var currentLength = currentLetters.length;
 var currentHidden = []
 
 //For loop that iterates through current number of letters and creates an equivalent array of '_' characters
-for (let i = 0; i < currentLength; i++){
+for (let i = 0; i < currentLength; i++) {
     currentHidden[i] = "_";
 }
 
@@ -29,12 +29,12 @@ var guessedArray = [];
 var numbersLeft = currentWord.length;
 
 //Win or Lose conditions (reset to new game -- variables essentially reset)
-function reset(){
-    currentWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+function reset() {
+    currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     currentLetters = currentWord.split("");
     currentLength = currentLetters.length;
     currentHidden = []
-    for (let i = 0; i < currentLength; i++){
+    for (let i = 0; i < currentLength; i++) {
         currentHidden[i] = "_";
     }
     guessRemaining = 12;
@@ -43,7 +43,7 @@ function reset(){
 }
 
 //Load function on page load
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     //Create event that occurs when key is pressed (arrow function)
     document.addEventListener('keydown', event => {
         //Define local variables
@@ -51,60 +51,64 @@ document.addEventListener('DOMContentLoaded', function(){
         var charList = 'abcdefghijklmnopqrstuvwxyz';
         //Key that user pressed (translated to lowercase key)
         var userKey = event.key.toLowerCase();
-        
+
         //Conditionals
         //If the user presses a key outside of the accepted key pressed, alert them in textbox, do not run remaining conditions
-        if (charList.indexOf(userKey) === -1){
+        if (charList.indexOf(userKey) === -1) {
             document.getElementById("textBox").textContent = "Only letters of the alphabet are allowed!"
             return
         };
 
         //If the user presses a key they have already guessed, alert them in textbox, do not run remaining conditions
-        if(guessedArray.includes(userKey)){
+        if (guessedArray.includes(userKey)) {
             document.getElementById("textBox").textContent = "You've already guessed this letter!"
             return
         }
 
         //If the user still has guesses remaining, subtract a guess
-        if (guessRemaining!= 0){
-            guessRemaining = guessRemaining-1;
+        if (guessRemaining != 0) {
+            guessRemaining = guessRemaining - 1;
+            document.getElementById("textBox").textContent = "Try Again!"
         };
         //If guessed array is less than 12 letters (the max), push newly guessed letters into it
-        if (guessedArray.length < 12){
+        if (guessedArray.length < 12) {
             guessedArray.push(userKey);
         };
+        
         //For each new letter guessed, convert the corresponding '_' for currentLetters into the letter guessed
-        for (let i = 0; i < currentLength; i++){
-            if(userKey === currentLetters[i]){
-                    currentHidden[i] = userKey;
-                    currentLetters[i] = 0;
-                    numbersLeft = numbersLeft - 1;
+        for (let i = 0; i < currentLength; i++) {
+            if (userKey === currentLetters[i]) {
+                currentHidden[i] = userKey;
+                currentLetters[i] = 0;
+                numbersLeft = numbersLeft - 1;
+                document.getElementById("textBox").textContent = "Nice guess!"
             };
         };
+
         console.log('wordBefore: ' + currentWord);
 
         //If there are no more letters left, the user wins
-        if(numbersLeft === 0){
-            numWins = numWins+1;
+        if (numbersLeft === 0) {
+            numWins = numWins + 1;
             document.getElementById("currentWord").textContent = currentWord;
-            setTimeout(function() {
+            setTimeout(function () {
                 alert("You are an Animal Expert");
                 reset()
                 document.getElementById("currentWord").textContent = currentHidden;
                 document.getElementById("guessesRemaining").textContent = guessRemaining;
                 document.getElementById("lettersGuessed").textContent = guessedArray;
-            },10)
-            ;
+            }, 10)
+                ;
         }
         //Otherwise, if there are no guesses remaining, they lose
-        else if (guessRemaining === 0){
-            setTimeout(function() {
+        else if (guessRemaining === 0) {
+            setTimeout(function () {
                 alert("You are a Complete and Utter Failure");
                 reset()
                 document.getElementById("currentWord").textContent = currentHidden;
                 document.getElementById("guessesRemaining").textContent = guessRemaining;
                 document.getElementById("lettersGuessed").textContent = guessedArray;
-            },10)
+            }, 10)
         };
         console.log('wordAfter: ' + currentWord);
 
@@ -117,7 +121,7 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 //Assign all variable values to html elements
-function assignValue(){
+function assignValue() {
     document.getElementById("currentWord").innerHTML = currentHidden;
     document.getElementById("wins").innerHTML = numWins;
     document.getElementById("guessesRemaining").innerHTML = guessRemaining;
@@ -125,4 +129,4 @@ function assignValue(){
 
 //Run our assignValue Function
 assignValue();
-    
+
